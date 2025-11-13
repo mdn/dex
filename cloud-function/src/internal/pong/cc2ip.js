f1dfcae0c8774a690c8c5c43af6e2bb92b92ef21
Claas Augner <caugner@mozilla.com>
2025-11-13 10:08:21 +0100

    style(eslint): disable @typescript-eslint/ban-ts-comment selectively

--- cloud-function/src/internal/pong/cc2ip.js
+++ cloud-function/src/internal/pong/cc2ip.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 export const cc2ip = {
   AD: "194.158.64.0",
--- cloud-function/src/internal/pong/pong2.js
+++ cloud-function/src/internal/pong/pong2.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import he from "he";
 import anonymousIpByCC from "./cc2ip.js";
